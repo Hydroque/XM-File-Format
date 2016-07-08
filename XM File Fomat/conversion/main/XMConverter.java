d0d845c2e4cc197177f959b82dec3c279e6b6d99
Hydro <Hydro@Hydro>
2016-07-08 07:39:24 -0400

    crappily added functionality to write and read xm, ignores compression

--- XM File Fomat/conversion/main/XMConverter.java
+++ XM File Fomat/conversion/main/XMConverter.java
@@ -29,29 +29,30 @@ import hydroque.compression.XMPacker;
 import hydroque.image.XMLoader;
 import hydroque.image.XMWriter;
 import hydroque.image.data.Image;
-import hydroque.image.data.Mipmap;
 
 public class XMConverter {
 
 	/**
 	 * Args
-	 * 0	1			2
-	 * xmi destination {files, ...}
-	 * xmi destination folder
+	 * 0	1		2			3			4
+	 * xmi	format	compression	destination	{files, ...}
+	 * xmi	format	compression	destination	folder
 	 * 
-	 * xmm destination {files, ...}
-	 * xmm destination folder
+	 * -- not supported --
+	 * xmm	format	compression	destination {files, ...}
+	 * xmm	format	compression	destination	folder
 	 * 
 	 * xmp destination {files, ...}
 	 * xmp destination folder
 	 * 
-	 * dexmp source destination
+	 * dexmp destination source
 	 * 
 	 * Examples:
 	 * xmi C:\Users\User\Desktop C:\Images\png\example.png
 	 * xmi C:\Users\User\Desktop C:\Images\png\example.png C:\Images\png\example2.png
 	 * xmi C:\Users\User\Desktop C:\Images\png
 	 * 
+	 * -- not supported --
 	 * xmm C:\Users\User\Desktop C:\Images\png\example.png C:\Images\png\example2.png
 	 * xmm C:\Users\User\Desktop C:\Images\png\
 	 * 
@@ -59,7 +60,7 @@ public class XMConverter {
 	 * xmp C:\Users\User\Desktop\ C:\Images\xm\example.xmi C:\Images\xm\example2.xmm
 	 * xmp C:\Users\User\Desktop\ C:\Images\xm\
 	 * 
-	 * dexmp C:\Users\User\Desktop\packed_xm.xmp C:\Users\User\Desktop\output
+	 * dexmp C:\Users\User\Desktop\output C:\Users\User\Desktop\packed_xm.xmp
 	 * 
 	 * vxmi C:\xm\example.xmi
 	 * 
@@ -74,12 +75,12 @@ public class XMConverter {
 		switch(args[0].toLowerCase()) {
 		case "xmi": // convert PNG to XMI
 			try {
-				final File desti = new File(args[1]);
+				final File desti = new File(args[3]);
 				if(!desti.exists() || !desti.isDirectory()) {
 					System.err.println("Error: destination is not a valid directory. No files generated.");
 					return;
 				}
-				final File source = new File(args[2]);
+				final File source = new File(args[4]);
 				if(!source.exists()) {
 					System.err.println("Error: source is not a valid directory or file. No files generated.");
 					return;
@@ -91,22 +92,43 @@ public class XMConverter {
 						&&  pathname.getName().split(Pattern.quote("."))[1].equalsIgnoreCase("png")
 					);
 				} else {
-					files = new File[args.length - 2];
-					for (int i=2; i<args.length; i++) {
-						files[i-2] = new File(args[i]);
-						if(!files[i-2].exists() || files[i-2].isDirectory()
+					files = new File[args.length - 4];
+					for (int i=4; i<args.length; i++) {
+						files[i-4] = new File(args[i]);
+						if(!files[i-4].exists() || files[i-4].isDirectory()
 								|| !args[i].split(Pattern.quote("."))[1].equalsIgnoreCase("png")) {
 							System.err.println("Error: file " + args[i] + " is not a valid file. No fles generated.");
 							return;
 						}
 					}
 				}
-				System.out.println("Files: " + files.length);
 				for (final File f : files) {
 					final PNGDecoder decoder = new PNGDecoder(f, PNGDecoder.Format.RGBA);
 					final ByteBuffer bb = ByteBuffer.allocate(decoder.getWidth() * decoder.getHeight() * 4);
 					final Image data = XMLoader.loadImagePNG(decoder, bb);
-					XMWriter.writeXMI(data, new File(desti, f.getName() + ".xmi"));
+					
+					final int size = data.getWidth() * data.getHeight();
+					Image out = null;
+					byte[] actual_body = null;
+					if(args[1].equals("1")) {
+						actual_body = new byte[size];
+						for (int i=0; i<size * 4; i+=4)
+							actual_body[i/4] = data.getBody()[i];
+						out = new Image(data.getWidth(), data.getHeight(), Image.MONOCHROME, Image.NONE, actual_body);
+					}
+					if(args[1].equals("3")) {
+						actual_body = new byte[size * 3];
+						int a = 0;
+						for (int i=0; i<size * 4; i++) {
+							actual_body[a++] = data.getBody()[i++];
+							actual_body[a++] = data.getBody()[i++];
+							actual_body[a++] = data.getBody()[i++];
+						}
+						out = new Image(data.getWidth(), data.getHeight(), Image.RGB, Image.NONE, actual_body);
+					}
+					if(args[1].equals("4"))
+						out = data;
+					XMWriter.writeXMI(out, new File(desti, f.getName() + ".xmi"));
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -114,12 +136,12 @@ public class XMConverter {
 			break;
 		case "xmm": // Convert PNGs or XMIs to XMM
 			try {
-				final File desti = new File(args[1]);
+				final File desti = new File(args[3]);
 				if(!desti.exists() || !desti.isDirectory()) {
 					System.err.println("Error: destination is not a valid directory. No files generated.");
 					return;
 				}
-				final File source = new File(args[2]);
+				final File source = new File(args[4]);
 				if(!source.exists()) {
 					System.err.println("Error: source is not a valid directory or file. No files generated.");
 					return;
@@ -135,11 +157,11 @@ public class XMConverter {
 									);
 					});
 				} else {
-					files = new File[args.length - 2];
-					for (int i=2; i<args.length; i++) {
-						files[i-2] = new File(args[i]);
+					files = new File[args.length - 4];
+					for (int i=4; i<args.length; i++) {
+						files[i-4] = new File(args[i]);
 						final String[] split = args[i].split(Pattern.quote("."));
-						if(!files[i-2].exists() || files[i-2].isDirectory()
+						if(!files[i-4].exists() || files[i-4].isDirectory()
 								|| (   !split[1].equalsIgnoreCase("png")
 									&& !split[1].equalsIgnoreCase("xmi"))) {
 							System.err.println("Error: file " + args[i] + " is not a valid file. No fles generated.");
@@ -147,18 +169,39 @@ public class XMConverter {
 						}
 					}
 				}
-				final Image[] data = new Image[files.length];
+				final Image[] images = new Image[files.length];
 				for (int i=0; i<files.length; i++) {
 					final String file_type = files[i].getName().split(Pattern.quote("."))[1];
 					if(file_type.equalsIgnoreCase("png")) {
 						final PNGDecoder decoder = new PNGDecoder(files[i], PNGDecoder.Format.RGBA);
 						final ByteBuffer bb = ByteBuffer.allocate(decoder.getWidth() * decoder.getHeight() * 4);
-						data[i] = XMLoader.loadImagePNG(decoder, bb);
+						
+						final Image data = XMLoader.loadImagePNG(decoder, bb);
+						final int size = data.getWidth() * data.getHeight();
+						byte[] actual_body = null;
+						if(args[1].equals("1")) {
+							actual_body = new byte[size];
+							for (int a=0; a<size * 4; a+=4)
+								actual_body[a/4] = data.getBody()[a];
+							images[i] = new Image(data.getWidth(), data.getHeight(), Image.MONOCHROME, Image.NONE, actual_body);
+						}
+						if(args[1].equals("3")) {
+							actual_body = new byte[size * 3];
+							int a = 0;
+							for (int j=0; j<size * 4; j++) {
+								actual_body[a++] = data.getBody()[j++];
+								actual_body[a++] = data.getBody()[j++];
+								actual_body[a++] = data.getBody()[j++];
+							}
+							images[i] = new Image(data.getWidth(), data.getHeight(), Image.RGB, Image.NONE, actual_body);
+						}
+						if(args[1].equals("4"))
+							images[i] = data;
 					}
 					if(file_type.equalsIgnoreCase("xmi"))
-						data[i] = XMLoader.loadImageXMI(files[i]);
+						images[i] = XMLoader.loadImageXMI(files[i]);
 				}
-				XMWriter.writeXMM(data, new File(desti, files[0].getName() + ".xmm"));
+				XMWriter.writeXMM(images, new File(desti, files[0].getName() + ".xmm"));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -183,7 +226,7 @@ public class XMConverter {
 					for (int i=2; i<args.length; i++) {
 						files[i-2] = new File(args[i]);
 						if(!files[i-2].exists() || files[i-2].isDirectory()) {
-							System.err.println("Error: file " + args[i] + " is not a valid file. No fles generated.");
+							System.err.println("Error: file " + args[i] + " is not a valid file. No files generated.");
 							return;
 						}
 					}
@@ -205,7 +248,9 @@ public class XMConverter {
 				final Image img = XMLoader.loadImageXMI(args[1]);
 				System.out.println("Width: " + img.getWidth());
 				System.out.println("Height: " + img.getHeight());
-				System.out.println("Transparency: " + img.hasTransparency());
+				System.out.println("ColorModel: " + img.getColorModel());
+				System.out.println("Compression: " + img.getCompression());
+				System.out.println("Data Size: " + img.getBody().length);
 			} catch (IOException e) {
 				System.err.println("Couldn't load/verify XMI file.");
 				e.printStackTrace();
@@ -213,15 +258,16 @@ public class XMConverter {
 			break;
 		case "vxmm": // verify XMM file
 			try {
-				final Mipmap map = XMLoader.loadImageXMM(args[1]);
-				System.out.println("Mipmap Levels: " + map.getLevels());
-				System.out.println("Global Transparency: " + map.hasTransparency());
-				for(int i=0; i<map.getImages().length; i++) {
-					final Image sub = map.getImages()[i];
+				final Image[] map = XMLoader.loadImageXMM(args[1]);
+				System.out.println("Mipmap Levels: " + map.length);
+				System.out.println("Definitive color model: " + map[0].getColorModel());
+				System.out.println("Definitive compression: " + map[0].getCompression());
+				for(int i=0; i<map.length; i++) {
+					final Image sub = map[i];
 					System.out.println("Sub Image " + i);
 					System.out.println("Width: " + sub.getWidth());
 					System.out.println("Height: " + sub.getHeight());
-					System.out.println("Local Transparency: " + sub.hasTransparency());
+					System.out.println("Data size: " + sub.getBody().length);
 				}
 			} catch (IOException e) {
 				System.err.println("Couldn't load/verify XMM file.");
